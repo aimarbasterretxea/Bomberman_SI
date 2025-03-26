2d0557ba3396c5dfef717663d306ff1516c8e35e
aimarbasterretxea <aimarbasterretxea.uni@gmail.com>
2025-03-26 12:50:22 +0100

    ya eztakitx ze ipini

---
+++ Bomberman/src/Eredua/BombermanBeltza.java
@@ -0,0 +1,15 @@
+package Eredua;
+
+public class BombermanBeltza extends Bomberman {
+	//Eraikitzailea
+	public BombermanBeltza() {
+		super();
+	}
+
+	@Override
+	public void bombaJarri() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Bomberman/src/Eredua/bombermanFactory.java
@@ -0,0 +1,28 @@
+package Eredua;
+
+public class bombermanFactory {
+	private bombermanFactory nirebombermanFactory;
+	
+	private bombermanFactory() {
+	}
+	
+	public bombermanFactory getbombermanFactory() {
+		if (nirebombermanFactory == null) {
+			nirebombermanFactory = new bombermanFactory();
+		}
+		return nirebombermanFactory;
+	}
+	
+	public Bomberman sortuBomberman(String pMota) {
+		Bomberman nireBomberman = null;
+		if(pMota.equals("BombermanBeltza")) {
+			nireBomberman = new BombermanBeltza();
+		}
+		else if(pMota.equals("BombermanGorria")) {
+			nireBomberman = new BombermanZuria();
+		}
+		
+		return nireBomberman;
+		
+	}
+}
